16d578e89715e64fc1a9cf85c0edb0de8e7100cd
javier briones <javier.brms@gmail.com>
2021-10-01 13:36:14 -0300

    creacion de primera pagina con HttpResponse con Request

---
+++ Django/ProyectoDjango/Proyecto1/Proyecto1/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def saludo(request):
+    return HttpResponse("Hola alumnos esta es nuestra primera pagina con Django")
+
+def despedida(request):
+    return HttpResponse("Hasta luego alumnos de django")
